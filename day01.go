98e51f4d07792e1f43f97391abc59508573ef827
Jonathan W. Taylor <elderling@gmail.com>
2022-12-03 08:26:06 -0600

    Read slice of slices from input file

--- day01.go
+++ day01.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func ReadElfCalories(filename string) [][]int {
+	elf_calories := [][]int{}
+
+	f, err := os.Open(filename)
+	if err != nil {
+		panic("couldn't open file")
+	}
+
+	input := bufio.NewScanner(f)
+
+	single_elf := []int{}
+	for input.Scan() {
+		line := input.Text()
+
+		if line == "" {
+			fmt.Println("Blank!")
+			elf_calories = append(elf_calories, single_elf)
+			single_elf = []int{}
+		} else {
+			fmt.Println(line)
+			theInt, _ := strconv.Atoi(input.Text())
+
+			single_elf = append(single_elf, theInt)
+		}
+	}
+
+	return elf_calories
+}
--- day01_test.go
+++ day01_test.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestReadElfCalories(t *testing.T) {
+	elf_calories := ReadElfCalories("test_data_day01a.txt")
+
+	if elf_calories[0][0] != 1000 {
+		t.Error("didn't populate slice of slices correctly.")
+	}
+}
