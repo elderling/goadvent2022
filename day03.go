2358d7d1f78b68b3411115da2bcf14a7c549ef2b
Jonathan W. Taylor <elderling@gmail.com>
2022-12-06 17:14:29 -0600

    Add code to read strings from file

--- day03.go
+++ day03.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"bufio"
+	"os"
+)
+
+func ReadRucksackStrings(filename string) []string {
+	rucksacks := []string{}
+
+	f, err := os.Open(filename)
+
+	if err != nil {
+		panic("Unable to read file")
+	}
+
+	input := bufio.NewScanner(f)
+
+	for input.Scan() {
+		rucksacks = append(rucksacks, input.Text())
+	}
+
+	return rucksacks
+
+}
--- day03_test.go
+++ day03_test.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestReadRucksackStrings(t *testing.T) {
+	rucksacks := ReadRucksackStrings("test_data_day03a.txt")
+
+	if rucksacks[0] != "vJrwpWtwJgWrhcsFMMfFFhFp" {
+		t.Error("First line not read properly")
+	}
+
+	if rucksacks[5] != "CrZsJsPPZsGzwwsLwLmpwMDw" {
+		t.Error("Last line not read properly")
+	}
+}
