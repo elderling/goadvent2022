42359f499d8d298dab3740644c4966e392dc5b81
Jonathan W. Taylor <elderling@gmail.com>
2022-12-24 11:24:47 -0600

    Add some init functions

--- day05.go
+++ day05.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	"bufio"
+	"regexp"
+)
+
+func ScanStartingStack(scanner *bufio.Scanner) []string {
+	s := []string{}
+
+	var matchNumeric = regexp.MustCompile(` \d `)
+
+	for scanner.Scan() {
+		t := scanner.Text()
+
+		s = append([]string{t}, s...)
+
+		if matchNumeric.MatchString(t) {
+			break
+		}
+	}
+
+	return s
+}
+
+func InitStackBottoms(s string) [][]string {
+	stacks := [][]string{}
+
+	for _, v := range StackStringToSlice(s) {
+		stack := []string{v}
+		stacks = append(stacks, stack)
+	}
+
+	return stacks
+}
+
+func InitTheRestOfTheStacks(stacks [][]string, theRest []string) {
+	for _, val := range theRest {
+		spec := StackStringToSlice(val)
+
+		for i, theVal := range spec {
+			if theVal != " " {
+				stacks[i] = append(stacks[i], theVal)
+			}
+		}
+	}
+}
+
+func StackStringToSlice(s string) []string {
+	stackString := []string{}
+
+	for i := 1; i < len(s); i = i + 4 {
+		stackString = append(stackString, string(s[i]))
+	}
+
+	return stackString
+}
+
+func MoveSingleCrate(stacks [][]string, from int, to int) {
+	stacks[to] = append(stacks[to], stacks[from][len(stacks[from])-1])
+	stacks[from] = stacks[from][:len(stacks[from])-1]
+}
+
+/*
+func GetStack
+func PushStack
+func PopStack
+
+*/
--- day05_test.go
+++ day05_test.go
@@ -1,1 +1,68 @@
 package main
+
+import (
+	"bufio"
+	"strings"
+	"testing"
+)
+
+const TEST_DATA_DAY_05a = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 `
+
+func TestScanStartingStacks(t *testing.T) {
+	scanner := bufio.NewScanner(strings.NewReader(TEST_DATA_DAY_05a))
+
+	theSlice := ScanStartingStack(scanner)
+
+	if theSlice[0] != " 1   2   3 " {
+		t.Error("First Line busted")
+	}
+
+	if theSlice[3] != "    [D]    " {
+		t.Error("Last Line busted")
+	}
+}
+
+func TestInitStackBottoms(t *testing.T) {
+	s := "[Z] [M] [P]"
+
+	stacks := InitStackBottoms(s)
+
+	if stacks[0][0] != "Z" {
+		t.Error("nope 0")
+	}
+
+	if stacks[1][0] != "M" {
+		t.Error("nope 1")
+	}
+
+	if stacks[2][0] != "P" {
+		t.Error("nope 2")
+	}
+}
+
+func TestInitTheRestOfTheStacks(t *testing.T) {
+	s := "[Z] [M] [P]"
+	stacks := InitStackBottoms(s)
+
+	theRest := []string{
+		"[N] [C]",
+		"    [D]",
+	}
+
+	InitTheRestOfTheStacks(stacks, theRest)
+
+	if stacks[0][0] != "Z" {
+		t.Error("Corruption!")
+	}
+
+	if stacks[0][1] != "N" {
+		t.Error("Corruption!")
+	}
+
+	if stacks[1][1] != "C" {
+		t.Error("Corruption!")
+	}
+}
